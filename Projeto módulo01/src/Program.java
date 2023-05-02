1581f71b97436b9d9d10667ca0ecbec6f6bcdd80
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-05-02 00:20:57 -0300

    feat(main): Criado função de deposito e crianção de conta no registro

--- Projeto módulo01/src/Program.java
+++ Projeto módulo01/src/Program.java
@@ -1,11 +1,11 @@
+import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class Program {
     static Scanner sc = new Scanner(System.in);
     static int numUsers = 1;
-
     static int userId = 0;
-    static String[][] usuario = new String[100][6];
+    static String[][] usuario = new String[100][7];
 
 
     public static void main(String[] args) {
@@ -16,6 +16,8 @@ public class Main {
         usuario[0][3] = "senha";
         usuario[0][4] = "conta";
         usuario[0][5] = "saldo";
+        usuario[0][6] = "idConta";
+
 
         while (true) {
             System.out.println("===== Jornada Bank =====");
@@ -40,26 +42,19 @@ public class Main {
                     System.out.println("Opção inválida! Tente novamente.");
             }
         }
-
-
     }
-
 
     public static void logar() {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Digite seu e-mail: ");
-        String email = input.next();
+        String email = input.nextLine();
         System.out.println("Digite sua senha: ");
-        String senha = input.next();
+        String senha = input.nextLine();
 
         if (numUsers > 1) {
             if (validaLogin(email, senha)) {
-                if (usuario[userId][4] == null) {
-                    criarConta();
-                } else {
-                    logado();
-                }
+               logado();
             }
         } else {
             System.out.println("Usuário inválido!");
@@ -87,25 +82,27 @@ public class Main {
         usuario[numUsers][1] = cpf;
         usuario[numUsers][2] = email;
         usuario[numUsers][3] = senha;
+        usuario[numUsers][4] = String.valueOf(new Random(0).nextInt(9999));
+        usuario[numUsers][5] = "0";
+        usuario[numUsers][6] = String.valueOf(numUsers);
         numUsers++;
 
         System.out.println("Usuário cadastrado com sucesso!");
     }
 
     public static boolean validaLogin(String email, String senha) {
-        for (int i = 1; i <= numUsers; i++) {
+        for (int i = 1; i < numUsers; i++) {
             if (usuario[i][2].equals(email) && usuario[i][3].equals(senha)) {
-                System.out.println("Bem vindo!!");
                 userId = i;
-            } else {
-                System.out.println("E-mail ou senha inválidos!!");
+                return true;
             }
         }
+        System.out.println(userId > 0 ? "Bem vindo!!" : "E-mail ou senha inválidos!!");
         return false;
     }
 
     public static boolean validaRegistro(String cpf, String email) {
-        for (int i = 1; i <= numUsers; i++) {
+        for (int i = 1; i < numUsers; i++) {
             if (usuario[i][1].equals(cpf) || usuario[i][2].equals(email)) {
                 System.out.println("Usuário já cadastrado!");
                 return true;
@@ -138,17 +135,26 @@ public class Main {
                 break;
             case 4:
                 System.out.println("Retornando a página de login!!");
-                break;
+                return;
             default:
                 System.out.println("Opção inválida!");
         }
     }
 
-    public static void criarConta() {
+    private static void fazerDeposito() {
+        System.out.print("Digite o valor do deposito: ");
+        double valorDeposito = sc.nextDouble();
 
-    }
-
-    public static void fazerDeposito() {
+        if(valorDeposito <= 0){
+            System.out.println("Valor inválido");
+        }
+        else {
+            usuario[userId][5] = String.valueOf(Double.parseDouble(usuario[userId][5]) + valorDeposito);
+            System.out.println("Depósitos realizado com sucesso.");
+            System.out.println("Saldo atual: " + String.format(usuario[userId][5]));
+            System.out.println();
+        }
+        logado();
     }
 
     public static void fazerSaque() {
