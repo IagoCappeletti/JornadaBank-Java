065a7935b4ce4b1b38e668f2bcd76420ca195a45
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-05-02 23:18:04 -0300

    feat(main): Criado função de saque e transferência

--- Projeto módulo01/src/Program.java
+++ Projeto módulo01/src/Program.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -25,8 +26,9 @@ public class Program {
             System.out.println("#2 - Cadastrar usuário");
             System.out.println("#3 - Sair do aplicativo");
             System.out.println("==========================");
-            System.out.println("Qual opção você deseja acessar? ");
+            System.out.print("Qual opção você deseja acessar? ");
             int opcao = sc.nextInt();
+            System.out.println();
 
             switch (opcao) {
                 case 1:
@@ -47,10 +49,11 @@ public class Program {
     public static void logar() {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Digite seu e-mail: ");
+        System.out.print("Digite seu e-mail: ");
         String email = input.nextLine();
-        System.out.println("Digite sua senha: ");
+        System.out.print("Digite sua senha: ");
         String senha = input.nextLine();
+        System.out.println();
 
         if (numUsers > 1) {
             if (validaLogin(email, senha)) {
@@ -63,13 +66,13 @@ public class Program {
 
     public static void registrarUsuario() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite seu nome completo: ");
+        System.out.print("Digite seu nome completo: ");
         String nome = sc.nextLine();
-        System.out.println("Digite seu CPF: ");
+        System.out.print("Digite seu CPF: ");
         String cpf = sc.next();
-        System.out.println("Digite seu e-mail: ");
+        System.out.print("Digite seu e-mail: ");
         String email = sc.next();
-        System.out.println("Digite uma senha: ");
+        System.out.print("Digite uma senha: ");
         String senha = sc.next();
 
         if (numUsers > 1) {
@@ -87,7 +90,7 @@ public class Program {
         usuario[numUsers][6] = String.valueOf(numUsers);
         numUsers++;
 
-        System.out.println("Usuário cadastrado com sucesso!");
+        System.out.println("Usuário cadastrado com sucesso!\n");
     }
 
     public static boolean validaLogin(String email, String senha) {
@@ -120,8 +123,9 @@ public class Program {
         System.out.println("#3 - Fazer uma transferência");
         System.out.println("#4 - Sair");
         System.out.println("==========================");
-        System.out.println("Qual opção você deseja acessar? ");
+        System.out.print("Qual opção você deseja acessar? ");
         int opcao = input.nextInt();
+        System.out.println();
 
         switch (opcao) {
             case 1:
@@ -142,7 +146,9 @@ public class Program {
     }
 
     private static void fazerDeposito() {
-        System.out.print("Digite o valor do deposito: ");
+        Locale.setDefault(Locale.US);
+        System.out.println("Saldo inicial: R$ " + String.format("%.1f",Double.parseDouble(usuario[userId][5])));
+        System.out.print("Digite o valor do deposito: R$ ");
         double valorDeposito = sc.nextDouble();
 
         if(valorDeposito <= 0){
@@ -151,15 +157,57 @@ public class Program {
         else {
             usuario[userId][5] = String.valueOf(Double.parseDouble(usuario[userId][5]) + valorDeposito);
             System.out.println("Depósitos realizado com sucesso.");
-            System.out.println("Saldo atual: " + String.format(usuario[userId][5]));
+            System.out.println("Saldo atual: R$ " + String.format("%.2f",Double.parseDouble(usuario[userId][5])));
             System.out.println();
         }
         logado();
     }
 
     public static void fazerSaque() {
+        Locale.setDefault(Locale.US);
+        System.out.println("Saldo dispónivel em conta: R$ "+ String.format("%.2f",Double.parseDouble(usuario[userId][5])));
+        System.out.print("Quanto você quer sacar? R$ ");
+        double valorSaque = sc.nextDouble();
+
+        if (valorSaque <= 0) {
+            System.out.println("Valor inválido!\n");
+        } else if (valorSaque > Double.parseDouble(usuario[userId][5])) {
+            System.out.println("O valor selecionado é maior que o saldo disponível!\n");
+        } else {
+            double novoSaldo = Double.parseDouble(usuario[userId][5]) - valorSaque;
+            usuario[userId][5] = Double.toString(novoSaldo);
+            System.out.println("Saque realizado com sucesso.");
+            System.out.println("Novo saldo siponível: R$ " + String.format("%.2f",Double.parseDouble(usuario[userId][5])));
+            System.out.println();
+        }
+        logado();
     }
 
     public static void fazerTransferencia() {
+        Locale.setDefault(Locale.US);
+        System.out.println("Saldo dispónivel em conta: R$ "+ String.format("%.2f",Double.parseDouble(usuario[userId][5])));
+        System.out.print("Qual é o valor da transferência? ");
+        double valorTransferencia = sc.nextDouble();
+        System.out.print("Para qual Id você quer transferir?");
+        String id = sc.next();
+        System.out.println(userId);
+        System.out.println(id);
+
+        if (valorTransferencia <= 0){
+            System.out.println("Você não tem saldo suficiente para essa transferência. Saldo disponível: " + usuario[userId][5]);
+        } else if (valorTransferencia > Double.parseDouble(usuario[userId][5])) {
+            System.out.println("O valor selecionado é maior que o saldo disponível!");
+        } else {
+            double novoSaldo = Double.parseDouble(usuario[userId][5]) - valorTransferencia;
+            usuario[userId][5] = Double.toString(novoSaldo);
+            for (int i = 0; i < numUsers; i++) {
+                if (usuario[i][6].equals(id)){
+                    double novoSaldoDestino = Double.parseDouble(usuario[i][5]) + valorTransferencia;
+                    usuario[i][5] = Double.toString(novoSaldoDestino);
+                    userId = i;
+
+                }
+            }
+        }
     }
 }
